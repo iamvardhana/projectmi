22ae263519a44f02a0fd657603d6ff95c79de6c6
Vardhana Navelker <iamvardhana@users.noreply.github.com>
2017-02-07 00:31:59 +0530

    Update doubly_ll.c

--- doubly_ll.c
+++ doubly_ll.c
@@ -1,164 +1,108 @@
 //doubly linked list
 #include<stdio.h>
 #include<malloc.h>
-struct node
-{
- int data;
-struct node * next;
- struct node *prev;
+
+struct node{
+    int data;
+    struct node *next;
+    struct node * prev;
 }*start=NULL;
 
-struct node * new_node()
-{
-    int data;
-    static struct node *nn;
-    printf("\nEnter the data_> ");
-    scanf("%d",&data);
-    nn=(struct node *)malloc(sizeof(struct node));
-    nn->data=data;
-    return nn;
-}
-
-void insert()
-{
- int n;
-  struct node *newnode;
-  newnode=new_node();
- printf("\nEnter>\n1.insert at end\n2>insert at beginning\n3>insert at a position.\n>");
- scanf("%d",&n);
- switch(n)
- {
-  case 1:
-
-  if(start==NULL)
-  {
-  newnode->next=NULL;
-  newnode->prev=NULL;
-  start=newnode;
-  }
-  else if(start->next==NULL)
-  {
-   start->next=newnode;
-   newnode->prev=start;
-   newnode->next=NULL;
-  }
-  else
-  {
-   struct node *temp;
-  temp->next=newnode;
-  newnode->prev=temp;
-  newnode->next=NULL;
-  }
-//  case 2:
-//  case 3;
-
-
-
- }
-
-
-}
-
-void del()
-{
-
-
-
-
-}
-
-void deallocate_mem()
-{
-
-
-
-}
-
-void reverse()
-{
-
-
-
-}
-
-void search( int data)
-{
-
-
-
-
-}
-
-void display()
-{
-int choice,i;
-struct node *temp;
+struct node* makenode(int a){
+    struct node *newnode;
+    newnode=(struct node *)malloc (sizeof(*newnode));
+    newnode->data=a;
+    return newnode;
+};
+void addnode_end(){
+int x;
+struct node * newnode,*temp;
 temp=start;
-    if(!temp)
-        printf("\nList is empty\n");
-    else
-    {
-        printf("\nEnter choice\n1>Display first node\n2>Display last node\n3>Display node at selected index\n4>Display all nodes\n");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1: printf("\nnode : 0\t\tdata : %d\n",start->data);
-                    break;
-//            case 2: temp=find_end(&choice);
-//                    printf("\nnode : %d\t\tdata : %d\n",choice-1,temp->data);
-//                    break;
-            case 3: printf("\nEnter index of node to display_> ");
-                    scanf("%d",&choice);
-                    find_end(&i);
-                    if(choice>=0 && choice<=i)
-                    {
-                        for(i=0;i<choice;i++)
-                            temp=temp->next;
-                        printf("\nnode : %d\t\tdata : %d\n",choice,temp->data);
-                    }
-                    else
-                        printf("\nIndex does not exist\n");
-                    break;
-//            case 4: display_nodes();
-//                    break;
-           default: printf("\nInvalid input\n");
-        }
+printf("\nEnter the data to be inserted: ");
+scanf("%d",&x);
+newnode = makenode(x);
+if(start==NULL){
+ newnode->next=NULL;
+ newnode->prev=NULL;
+ start=newnode;
+}
+else if(start->next==NULL){
+    start->next=newnode;
+    newnode->prev=start;
+    newnode->next=NULL;
+}
+else{
+ while(temp->next!=NULL)
+    temp=temp->next;
+ temp->next=newnode;
+ newnode->prev=temp;
+ newnode->next=NULL;
+}
+printf("\nData inserted at the end.\n");
+}
+
+void addnode_beg()
+{
+    int x;
+    struct node* newnode,*temp;
+    temp=start;
+    printf("\nEnter data to be inserted:");
+    scanf("%d",&x);
+    newnode=makenode(x);
+    if(start==NULL){
+        newnode->prev=NULL;
+        newnode->next=NULL;
+        start=newnode;
+    }
+    else {
+            start->prev=newnode;
+            newnode->next=start;
+            newnode->prev=NULL;
+            start=newnode;
     }
 
+}
 
+void display(){
+struct node * temp;
+int i=1;
+temp=start;
+if(temp==NULL)
+    printf("\nLinked list is empty.");
+else{
+    do{
+       {
+        printf("\n%d  %d",i,temp->data);
+        temp=temp->next;
+        i++;
+       }
+    }while(temp->next!=NULL);
 
+}
 
 }
 
 int main(){
+  int a,n;
+  while(1){
+  printf("\nEnter choice.\n1>add element.\n2>delete element.\n3>search element.\n4>display list.\n5>reverse list.\nany no to exit.\n");
+  scanf("%d",&n);
+  switch (n){
+         case 1:printf("\nEnter choice.\n1>At the end.\n2.at the beginning.\n3>at a specific position.\n");
+                scanf("%d",&n);
+                switch(n){
+                case 1:
+                   addnode_end();
+                   break;
+                case 2:
+                    addnode_beg();
+                    break;
+                }
+                break;
+          case 4: display();      //check working of display function.
+                  break;
+     }
 
-int choice;
-while(1)
-{
-printf("\nEnter the choice:\n1>Insert a node.\n2>Delete a node.\n3>display the list.");
-printf("\n4>searching for an element.\n5>reversing.\nany other no to exit.\n>");
-scanf("%d",&choice);
-switch(choice)
-{
-  case 1:insert();
-         break;
-  case 2:del();
-         break;
-  case 3:display();
-         break;
-  case 4:if(!start)
-           printf("\nThe list is empty.");
-         else
-         {
-         printf("\nenter element to be searched>");
-         scanf("%d",&choice);
-         search(choice);
-         }
-         break;
-  case 5: reverse();
-          break;
-  default:deallocate_mem();
-          return 0;
-}
-}
+  }
+
 }
